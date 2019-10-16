96697b0a56e6f976677de593316c9e0b014d53cb
David Fennell <david.fennell@ruralsource.com>
2019-10-16 13:46:02 -0400

    Cleaned up HTML pages and script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -15,132 +15,88 @@ var datePattern = /^(?!(?![02468][048]|[13579][26]00)..(?!(?!00)[02468][048]|[13
 
 
 //validate Date in any format--NOT WORKING
-function validateDate()  
-  {  
-var inputText = document.getElementById("searchBox").value;
-alert(inputText);
+function validateDate(){  
+	var inputText = document.getElementById("searchBox").value;
+	alert(inputText);
 
-  
-  var dateformat = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;  
-  // Match the date format through regular expression  
-  if(inputText.value == dateformat)  
-  { 
-
-alert("1st IF"); 
-  document.form1.text1.focus();  
-  //Test which seperator is used '/' or '-'  
-  var opera1 = inputText.value.split('/');  
-  var opera2 = inputText.value.split('-');  
-  lopera1 = opera1.length;  
-  lopera2 = opera2.length;  
-  // Extract the string into month, date and year  
-	  if (lopera1>1)  
-	  {  
-  alert("1st IF"); 
-
-	  var pdate = inputText.value.split('/');  
-	  }  
-	  else if (lopera2>1)  
-	  {  
-  
-  alert("1st IF"); 
-
-	  var pdate = inputText.value.split('-');  
-	  }  
-	  
-	  var dd = parseInt(pdate[0]);  
-	  var mm  = parseInt(pdate[1]);  
-	  var yy = parseInt(pdate[2]);  
-	  // Create list of days of a month [assume there is no leap year by default]  
-	  var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];  
-	  if (mm==1 || mm>2)  
-	  {  
-  alert("1st IF"); 
-
-		  if (dd>ListofDays[mm-1])  
-		  {  
-		  alert('Invalid date format!');  
-		  return false;  
-		  }  
-	  }  
-	  if (mm==2)  
-	  {  
-	  var lyear = false;  
-		  if ( (!(yy % 4) && yy % 100) || !(yy % 400))   
-		  {
-alert("1st IF"); 
-			  
-		  lyear = true;  
-		  }  
-		  if ((lyear==false) && (dd>=29))  
-		  { 
-alert("1st IF"); 
-	  
-		  alert('Invalid date format!');  
-		  return false;  
-		  }  
-		  if ((lyear==true) && (dd>29))  
-		  {  
-alert("1st IF"); 
-
-		  alert('Invalid date format!');  
-		  return false;  
-		  }  
-	  }  
-	  }  
-  else  
-  {  
-alert("Else"); 
-
-  alert("Invalid date format!");  
-  document.form1.text1.focus();  
-  return false;  
-  } 
-alert("End Function"); 
-  
-  }
-  
-  2006-10-10
-  10-10-2006
-  
-  1-10-2006
-  1-1-2006
-  
-  
-
-/^\d{1,2}\/\d{1-2}\/\d{4}$/.test(dateString) //10/10/2001
-
-/^\d{4}\/\d{1-2}\/\d{1-2}$/.test(dateString) //2001/1/1
-
-/^\d{4}\-\d{1-2}\-\d{1-2}$/.test(dateString) //2001-1-1
-
-/^\d{1-2}\-\d{1-2}\-\d{4}$/.test(dateString) //1-1-2001
-
-
- 
-  
-function dateAnyFormat(dateString){
+	var dateformat = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;  
 	
-	
-	// First check for the pattern
-    if(/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString)) // num{1or2} and num{1or2} and num{4}
-        {
-			var parts = dateString.split("/");
-			var day = parseInt(parts[1], 10);
-			var month = parseInt(parts[0], 10);
-			var year = parseInt(parts[2], 10
-			alert(month + day + year);
-		}
-	else if (/^\d{4}\/\d{1-2}\/\d{1-2}$/.test(dateString)) //2001/1/1
-		{
-			var parts = dateString.split("/");
-			var day = parseInt(parts[1], 10);
-			var month = parseInt(parts[2], 10);
-			var year = parseInt(parts[0], 10);
-		}
+	// Match the date format through regular expression  
+	if(inputText.value == dateformat){ 
+
+		document.form1.text1.focus();  
+	  
+		//Test which seperator is used '/' or '-'  
+		var opera1 = inputText.value.split('/');  
+		var opera2 = inputText.value.split('-');  
+		lopera1 = opera1.length;  
+		lopera2 = opera2.length;  
+		
+		//Extract the string into month, date and year  
+		if (lopera1>1){  
+			var pdate = inputText.value.split('/');  
+		}  
+		else if (lopera2>1){  
+			var pdate = inputText.value.split('-');  
+		}  
+		  
+		var dd = parseInt(pdate[0]);  
+		var mm  = parseInt(pdate[1]);  
+		var yy = parseInt(pdate[2]);  
+		  
+		// Create list of days of a month [assume there is no leap year by default]  
+		var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];  
+		
+		if (mm == 1 || mm > 2){  
+
+			if (dd>ListofDays[mm-1]){  
+				alert('Invalid date format!');  
+				return false;  
+			}  
+		}  
+		if (mm==2){  
+		  var lyear = false;  
+			  if ( (!(yy % 4) && yy % 100) || !(yy % 400)){ 
+				lyear = true;  
+			  }  
+			  if ((lyear==false) && (dd>=29)){ 
+				  alert('Invalid date format!');  
+				  return false;  
+				  
+			  }  
+			  if ((lyear==true) && (dd>29)){  
+				  alert('Invalid date format!');  
+				  return false;  
+			  }  
+		} 
+	}  
+    else{  
+		alert("Invalid date format!");  
+		document.form1.text1.focus();  
+		return false; 
+		
+	} 
 }
   
-// Validates that the input string is a valid date formatted as "mm/dd/yyyy"---WORKING
+function dateAnyFormat(dateString){
+
+	// First check for the pattern
+    if(/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString)){ // num{1or2} and num{1or2} and num{4}
+		var parts = dateString.split("/");
+		var day = parseInt(parts[1], 10);
+		var month = parseInt(parts[0], 10);
+		var year = parseInt(parts[2], 10
+		alert(month + day + year);
+	}
+	else if (/^\d{4}\/\d{1-2}\/\d{1-2}$/.test(dateString)){ //2001/1/1
+		var parts = dateString.split("/");
+		var day = parseInt(parts[1], 10);
+		var month = parseInt(parts[2], 10);
+		var year = parseInt(parts[0], 10);
+	}
+}
+  
+// Validates that the input string is a valid date formatted as "mm/dd/yyyy"
 function isValidDate(dateString)
 {
     // First check for the pattern
@@ -154,15 +110,17 @@ function isValidDate(dateString)
     var year = parseInt(parts[2], 10);
 
     // Check the ranges of month and year
-    if(year < 1000 || year > 3000 || month == 0 || month > 12)
+    if(year < 1000 || year > 3000 || month == 0 || month > 12){
         return false;
+	}
 
     var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
 
     // Adjust for leap years
-    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)){
         monthLength[1] = 29;
-
+	}
+	
     // Check the range of the day
     return day > 0 && day <= monthLength[month - 1];
 };
@@ -175,19 +133,17 @@ function validateSearch(){
 		alert("Email");
 		
 	}
-	if(datePattern.test(searchBox))
-	{
+	if(datePattern.test(searchBox)){
 		alert("date");
 	}
-
-
 }
 function searchAlert(){
 	
 	var searchBox = document.getElementById("searchBox").value;
-alert(searchBox);
+	alert(searchBox);
 }
 
+//Not needed
 function regEx(){
 
 }
@@ -199,14 +155,12 @@ function contactUsSubmit()
 	var firstNameField = document.getElementById("first_name").value;
 	var lastNameField = document.getElementById("last_name").value;
 	
-	if(/^[_a-zA-Z0-9\\-]+(\.[_a-zA-Z0-9\\-]+)*@[_a-zA-Z0-9\\-]+(\.[a-zA-Z0-9\\-]+)*(\.[a-z]{2,6})$/.test(inputEmail);
-	{
+	if(/^[_a-zA-Z0-9\\-]+(\.[_a-zA-Z0-9\\-]+)*@[_a-zA-Z0-9\\-]+(\.[a-zA-Z0-9\\-]+)*(\.[a-z]{2,6})$/.test(inputEmail){
 		var emailField = inputEmail;
 		alert(emailField);
 	}
 	
-	else
-	{
+	else{
 		alert("Invalid Email.");
 	}
 
@@ -215,6 +169,7 @@ function contactUsSubmit()
 function createEventListeners(){
 	var btn = document.getElementById("searchBtn");
 	var contactUsBtn = document.getElementById("contactUsSubmit");
+
 	if(btn.addEventListener){
 		btn.addEventListener("click", validateDate, false);
 	} else if (btn.attachEvent){
